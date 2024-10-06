c0073520e7fca9a9c3200b2f230aa22acd23b2e8
MatthewO1221 <matthewol1221@gmail.com>
2024-10-06 16:56:59 -0700

    Switching to subtractive generation

--- 24-25Year/Fall24/DES214/MapStuff/MapGenerator.gd
+++ 24-25Year/Fall24/DES214/MapStuff/MapGenerator.gd
@@ -6,7 +6,10 @@
 #Create Biome Mode Structs
 
 
-
+#Plan
+#Step 1: Generate Rooms Randomly Throughout Level
+#Step 2: Add all entrance tiles to branch list
+#Step 3: Let algorithm generate things from branches
 
 extends Node2D
 
@@ -22,7 +25,31 @@ extends Node2D
 #tiletype enum
 enum TileType {Empty = -1, Floor = 1, Wall = 2, Entrance = 3}
 
-#time to wait for slowing things down
+enum DungeonRooms
+{
+	Basic = 1
+}
+
+enum DensityModes
+{
+	Sparse,
+	Regular,
+	Cluttered
+}
+
+class Mode:
+	var name: String
+	var roomList: Dictionary
+	var roomAmount: DensityModes
+	var corridorList: Dictionary
+	var corridorAmount: DensityModes
+	var enemyList: Dictionary
+	var lootList: Dictionary
+
+
+class Region:
+	var name: String
+	var totalTiles: int
 
 
 #Maps the directions to numbers to make random generation easier
@@ -74,32 +101,103 @@ var branchList: Array
 
 func Generate() -> void:
 	
-		
-		
-	var placeTile = false
-	for i in range(10):
-		var dir = randi_range(0,3)
-		if IsValidNeighbor((currentTile), directionMap[dir]):
-			var roomMaybe = randf_range(0, 1.0)
-			if roomMaybe < roomChance:
-				SpawnRoom(currentTile)
-				currentTile = branchList.pop_front()
-			else:
-				currentTile = PlaceCorridor(TileType.Floor, currentTile, dir)
-			
-			#currentTile = GetCoordsInDir(currentTile, directionMap[dir])
-			placeTile = true
-			break
-			
-	if !placeTile || !currentTile || currentTile.x > mapSize || currentTile.x < -mapSize || currentTile.y > mapSize || currentTile.y < -mapSize:
-		if branchList.is_empty():
-			stillGenerating = false
-		else:
-			currentTile = branchList.pop_front()
+	var testMode = Mode.new()
+	testMode.name = "TestMode"
+	testMode.roomList = {testRoom = 0}
+	testMode.roomAmount = DensityModes.Regular
+	
+	var testRegion = Region.new()
+	testRegion.name = "TestRegion"
+	testRegion.totalTiles = mapSize ** 2
+	
+	SpawnRooms(testMode, testRegion)
+	
+	stillGenerating = false
+	
+	#var placeTile = false
+	#for i in range(10):
+		#var dir = randi_range(0,3)
+		#if IsValidNeighbor((currentTile), directionMap[dir]):
+			#var roomMaybe = randf_range(0, 1.0)
+			#if roomMaybe < roomChance:
+				#SpawnRoom(currentTile)
+				#currentTile = branchList.pop_front()
+			#else:
+				#currentTile = PlaceCorridor(TileType.Floor, currentTile, dir)
+			#
+			##currentTile = GetCoordsInDir(currentTile, directionMap[dir])
+			#placeTile = true
+			#break
+			#
+	#if !placeTile || !currentTile || currentTile.x > mapSize || currentTile.x < -mapSize || currentTile.y > mapSize || currentTile.y < -mapSize:
+		#if branchList.is_empty():
+			#stillGenerating = false
+		#else:
+			#currentTile = branchList.pop_front()
 		
 		
 
 		
+		
+func SpawnRooms(mode: Mode, region: Region) -> void:
+	var roomList = mode.roomList
+	var roomDensity = mode.roomAmount
+	var totalTiles = region.totalTiles
+	var tilesPlaced = 0
+	
+	while true:
+		
+		var placeTile = GetRandomTile(region)
+		
+		for i in range(10):
+			var roomToTry = GetRandomRoom(mode)
+			var rotation = TileTransform.values().pick_random()
+			var canBePlaced = CheckRoom(placeTile, roomToTry, rotation)
+			
+			if canBePlaced:
+				PlaceRoom(roomToTry, placeTile, rotation)
+				var tileCount = roomToTry.get_size().x * roomToTry.get_size().y
+				tilesPlaced += tileCount
+				break
+		
+		
+		
+		match roomDensity:
+			DensityModes.Sparse when float(tilesPlaced)/totalTiles >= 0.1:
+				break
+			DensityModes.Regular when float(tilesPlaced)/totalTiles >= 0.25:
+				break
+			DensityModes.Cluttered when float(tilesPlaced)/totalTiles >= 0.5:
+				break
+	
+	
+	
+	
+
+func GetRandomTile(region: Region) -> Vector2i:
+	var returnTile: Vector2i
+	
+	returnTile.x = randi_range(-mapSize, mapSize)
+	returnTile.y = randi_range(-mapSize, mapSize)
+	
+	return returnTile
+
+func GetRandomRoom(mode: Mode) -> TileMapPattern:
+	var patternsCount = mode.roomList.size()
+	
+	var rand = randi_range(0, patternsCount)
+	
+	var patternIndex = mode.roomList.values().pick_random()
+	
+	var pattern = tileMap.tile_set.get_pattern(patternIndex)
+	
+	return pattern
+	
+	
+func PlaceRoom(room: TileMapPattern, coords: Vector2i, _rotation: TileTransform) -> void:
+	tileMap.set_pattern(0, coords, room)
+	
+
 func PlaceTile(type: TileType, coords: Vector2i) -> void:
 	tileMap.set_cell(0, coords, 0, Vector2i(0,0), type)
 	var rando = randf_range(0, 1.0)
@@ -153,48 +251,49 @@ func GetCoordsInDir(coords: Vector2i, dir: Directions) -> Vector2i:
 	
 #region Room Code
 	
-func SpawnRoom(coords: Vector2i) -> void:
-	var roomToPlace = ChooseRoom(coords)
+#func SpawnRoom(coords: Vector2i) -> void:
+	#var roomToPlace = ChooseRoom(coords)
+	#
+	#if roomToPlace != null:
+		#tileMap.set_pattern(0, coords + roomToPlace.Offset, tileMap.tile_set.get_pattern(roomToPlace.Index))
+	#
+func CheckRoom(coords: Vector2i, room: TileMapPattern, _rotation: TileTransform) -> bool:
 	
-	if roomToPlace != null:
-		tileMap.set_pattern(0, coords + roomToPlace.Offset, tileMap.tile_set.get_pattern(roomToPlace.Index))
+	var roomTiles = room.get_used_cells()
+	var foundPlacement = false
 	
-func CheckRoom(coords: Vector2i, room: Room) -> bool:
-	
-	var roomToCheck = tileMap.tile_set.get_pattern(room.Index)
-	var roomTiles = roomToCheck.get_used_cells()
-	var placement = coords + room.Offset
 	
 	for i in roomTiles:
-		var curTile = tileMap.map_pattern(placement, i, roomToCheck)
+		var curTile = tileMap.map_pattern(coords, i, room)
 		if tileMap.get_cell_source_id(0, curTile) != TileType.Empty:
 			return false
-			
+		
 	return true
 	
-func ChooseRoom(coords: Vector2i) -> Room:
-	var returnRoom: Room
-	var patternsCount = tileMap.tile_set.get_patterns_count()
-	for i in range(10):
-		var randRoom = randi_range(0, patternsCount - 1)
-		returnRoom.Index = randRoom
-		if CheckRoom(coords, GetRoomShift(returnRoom)):
-			
-			return returnRoom
 	
-	return null
-	
-	
-func GetRoomShift(room: Room) -> Room:
-	var pattern = tileMap.tile_set.get_pattern(room.Index)
-	
-	for i in pattern.get_used_cells():
-		if pattern.get_cell_source_id(i) == TileType.Entrance:
-			room.Offset = i
-	
-	return room
-
-#endregion
+#func ChooseRoom(coords: Vector2i) -> Room:
+	#var returnRoom: Room
+	#var patternsCount = tileMap.tile_set.get_patterns_count()
+	#for i in range(10):
+		#var randRoom = randi_range(0, patternsCount - 1)
+		#returnRoom.Index = randRoom
+		#if CheckRoom(coords, GetRoomShift(returnRoom)):
+			#
+			#return returnRoom
+	#
+	#return null
+	#
+	#
+#func GetRoomShift(room: Room) -> Room:
+	#var pattern = tileMap.tile_set.get_pattern(room.Index)
+	#
+	#for i in pattern.get_used_cells():
+		#if pattern.get_cell_source_id(i) == TileType.Entrance:
+			#room.Offset = i
+	#
+	#return room
+#
+##endregion
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
